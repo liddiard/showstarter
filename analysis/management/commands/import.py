e3230cdc6dbc7b196fa9859576c38c5107c88a0a
omniaura <omniaura5@gmail.com>
2014-01-19 04:09:12 -0600

    fixed bugs in import script, increased CharField max lengths

--- analysis/management/commands/import.py
+++ analysis/management/commands/import.py
@@ -7,44 +7,43 @@ ENTRY_START = 297
 RATING_RANGE = (20, 24)
 TITLE_START = 26
 
-input_file = sys.argv[1]
+input_file = sys.argv[2]
 
 with open(input_file, 'r') as f:
-    entries = [line.strip() for line in f]
+    entries = [line.strip().decode('latin-1') for line in f]
 
 start = RATING_RANGE[0]
 end = RATING_RANGE[1]
 capturing = False
 prev_rating = None
 prev_show = None
-episode = r'{.*}'
+episode_re = r'{(?P<episode_name>.*?)\s+\(#(?P<season>\d+)\.(?P<episode_number>\d+)\)}'
 
-for entry in entries[ENTRY_START:ENTRY_START+100]:
+for entry in entries[ENTRY_START:]:
     try:
         rating = float(entry[start:end])
-        show_name = re.search(r'(.*?)\s+{', entry[TITLE_START:])
-        if show_name:
-            show_name = show_name.group(1)
-        episode_name = re.search(r'{(.*)}', entry[TITLE_START:])
-        if episode_name:
-            episode_name = episode_name.group(1)
-            episode_comp = re.search(r'\(#(\d+)\.(\d+)\)', episode_name)
-            season, episode = episode_comp.group(1), episode_comp.group(2)
+    except ValueError:
+        print "reached terminator"
+        break
+    else:
+        episode = re.search(episode_re, entry[TITLE_START:])
+        if episode:
+            episode_name = episode.group('episode_name')[:127]
+            season = episode.group('season')
+            episode_number = episode.group('episode_number')
         else:
-            episode_name = season = episode = None
+            show_name = entry[TITLE_START:][:127]
+            episode_name = season = episode_number = None
         # print "rating:", rating
         # print "show:", show_name
         # print "episode name:", episode_name
         # print "season:", season
-        # print "episode:", episode
+        # print "episode:", episode_number
         # print "\n"
-    except ValueError:
-        print "reached terminator"
-        break
     if capturing and episode:
-        pass
         # we're capturing and there's another episode to add to the current show
         e = Episode(name=episode_name, rating=rating, show=s, season=season, 
+                    episode=episode_number)
         e.save()
     elif capturing:
         # we're capturing and there's no match
@@ -54,7 +53,7 @@ for entry in entries[ENTRY_START:ENTRY_START+100]:
         s = Show(name=prev_show, rating=prev_rating)
         s.save()
         e = Episode(name=episode_name, rating=rating, show=s, season=season, 
-                    episode=episode)
+                    episode=episode_number)
         e.save()
         capturing = True
     prev_rating = rating
