879ca8f8e9e72d99d8c44f3680b3bda775fbe637
omniaura <omniaura5@gmail.com>
2014-01-19 10:17:59 -0600

    basic views skeleton, created templates, single show url routing and basic display

--- analysis/views.py
+++ analysis/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from .models import Show, Episode
+
+
+class FrontView(TemplateView):
+
+    template_name = "front.html"
+
+
+class SingleShowView(TemplateView):
+    
+    template_name = "show_single.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SingleShowView, self).get_context_data(**kwargs)
+        show = self.kwargs.get('show')
+        s = Show.objects.get(pk=show)
+        context['show'] = s
+        episodes = Episode.objects.filter(show=s).order_by('season', 'episode')
+        context['episodes'] = episodes
+        return context
+
+
+class ComparisonShowView(TemplateView):
+    
+    template_name = "show_comparison.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ComparisonShowView, self).get_context_data(**kwargs)
+        return context
--- showstarter/urls.py
+++ showstarter/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from analysis import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'showstarter.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.FrontView.as_view(), name='front'),
+    url(r'^show/(?P<show>\w+)/$', views.SingleShowView.as_view(), name='show'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
