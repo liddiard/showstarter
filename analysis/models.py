e45934ac43109faf92bd744eaf352ed4286ca6b5
omniaura <omniaura5@gmail.com>
2014-01-31 15:28:17 -0800

    abandoning slug generation because of the difficulty of making unique

--- analysis/models.py
+++ analysis/models.py
@@ -5,13 +5,7 @@ from django.template.defaultfilters import slugify
 class Show(models.Model):
     name = models.CharField(max_length=128)
     year = models.PositiveIntegerField(null=True, blank=True)
-    slug = models.SlugField(max_length=128, unique=True)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
-
-    def save(self, *args, **kwargs):
-        slug_str = "%s_%s" % (self.name, self.year)
-        self.slug = slugify(slug_str)
-        super(Show, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return "%s_(%s)" % (self.name, self.year)
