6ed2243b1d731aad453db5482a67329fa55bbf97
omniaura <omniaura5@gmail.com>
2014-01-31 15:12:58 -0800

    removed SlugField from Episode model, made Show slugs unique

--- analysis/models.py
+++ analysis/models.py
@@ -4,29 +4,25 @@ from django.template.defaultfilters import slugify
 
 class Show(models.Model):
     name = models.CharField(max_length=128)
-    slug = models.SlugField(max_length=128)
     year = models.PositiveIntegerField(null=True, blank=True)
+    slug = models.SlugField(max_length=128, unique=True)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(name)
+        slug_str = "%s_%s" % (self.name, self.year)
+        self.slug = slugify(slug_str)
         super(Show, self).save(*args, **kwargs)
 
     def __unicode__(self):
-        return "%s (%s)" % (self.name, self.year)
+        return "%s_(%s)" % (self.name, self.year)
 
 
 class Episode(models.Model):
     show = models.ForeignKey('Show')
     name = models.CharField(max_length=128, blank=True)
-    slug = models.SlugField(max_length=128)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
     season = models.PositiveIntegerField()
     episode = models.PositiveIntegerField()
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(name)
-        super(Episode, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return "(%s.%s) %s" % (self.season, self.episode, self.name)
